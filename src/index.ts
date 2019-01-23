23678e51350c9ffbd2f3e20567b290c1aa70b660
Matthew Allpress <matthew.allpress@gmail.com>
2019-01-23 12:15:06 +1030

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { Tokenizer } from "./tokenizer";
 import { Parser } from "./parser/index";
 
-
-let tokenizer = new Tokenizer();
-
-
-let tokens = tokenizer.tokenize("if (Decision \"A\" Outcome is equal to \"Leave As Is\" OR Pipe 'PipeA' Diameter is greater than or equal to -1.001) and pipe \"PipeB\" Diameter is less than 100 then decision \"a\" outcome is \"b\" and decision \"d\" outcome is \"c\"");
-console.log(JSON.stringify(tokens))
-let parser = new Parser();
-let res = parser.parse(tokens);
+export = {
+  Parser,
+  Tokenizer
+};
